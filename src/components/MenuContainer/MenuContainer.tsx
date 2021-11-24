b5487bde4c83b554534c1102e8f9f1fac31f9886
tishka25 <teodorstanishev@gmail.com>
2021-11-24 16:35:40 +0200

    update icons

--- src/components/MenuContainer/MenuContainer.tsx
+++ src/components/MenuContainer/MenuContainer.tsx
@@ -13,15 +13,15 @@ export default function MenuContainer(props: Props){
     return (
         <div className="menu-container">
             <MenuSection type="top">
-                <MenuItem icon={require("../../icons/New tab16px.svg").default} name="New tab"/>
-                <MenuItem icon={require("../../icons/New window 16px.svg").default} name="New window"/>
+                <MenuItem icon={require("../../icons/New tab 16px.svg").default} name="New tab"/>
+                <MenuItem icon={require("../../icons/New window icon 16px.svg").default} name="New window"/>
                 <MenuItem icon={require("../../icons/Incognito icon 16px.svg").default} name="New incognito window"/>
             </MenuSection>
             <MenuSection type="top">
                 Zoom section
             </MenuSection>
             <MenuSection type="top">
-                <MenuItem icon={require("../../icons/Bookmarks 16px .svg").default} name="Bookmarks"/>
+                <MenuItem icon={require("../../icons/Bookmarks 16px.svg").default} name="Bookmarks"/>
                 <MenuItem icon={require("../../icons/Reading list 16px.svg").default} name="Reading list"/>
                 <MenuItem icon={require("../../icons/History 16px.svg").default} name="History"/>
                 <MenuItem icon={require("../../icons/Downloads 16px.svg").default} name="Downloads"/>
@@ -39,18 +39,18 @@ export default function MenuContainer(props: Props){
             </MenuSection>
             <MenuSection title="Appearance">
                 <ThemeItem/>
-                <MenuItem icon={require("../../icons/Show Bookmarks 16px.svg").default} name="f-Show bookmarks bar"/>
+                <MenuItem icon={require("../../icons/Show bookrmarks bar 16 px.svg").default} name="Show bookmarks bar"/>
                 <MenuItem icon={require("../../icons/Home 16px.svg").default} name="Show home"/>
             </MenuSection>
             <MenuSection title="Start page">
-                <MenuItem icon={require("../../icons/Most visited sites 16px.svg").default} name="f-Most visited sites"/>
+                <MenuItem icon={require("../../icons/Most visited sites 16px.svg").default} name="Most visited sites"/>
                 <MenuItem icon={require("../../icons/Recent activity 16px.svg").default} name="Recent activity"/>
                 <MenuItem icon={require("../../icons/Show news icon 16px.svg").default} name="Show news"/>
             </MenuSection>
             <MenuSection title="Privacy settings">
-                <MenuItem icon={require("../../icons/Block ads 16 px.svg").default} name="f-Block ads"/>
-                <MenuItem icon={require("../../icons/Block trackers 16px.svg").default} name="f-Block trackers"/>
-                <MenuItem icon={require("../../icons/Clear browsing data 16x.svg").default} name="f-Clear browsing data"/>
+                <MenuItem icon={require("../../icons/Block ads 16 px.svg").default} name="Block ads"/>
+                <MenuItem icon={require("../../icons/Block trackers 16px.svg").default} name="Block trackers"/>
+                <MenuItem icon={require("../../icons/Clear browsing data 16px.svg").default} name="Clear browsing data"/>
                 <MenuItem icon={require("../../icons/Settings 16px.svg").default} name="Settings"/>
             </MenuSection>
             <MenuSection type='bottom'>
