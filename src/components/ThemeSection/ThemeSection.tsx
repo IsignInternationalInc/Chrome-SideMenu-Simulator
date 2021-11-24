27559a8ba1a1e8a0e8e7309b3c08a36f71fc5d2a
tishka25 <teodorstanishev@gmail.com>
2021-11-24 17:02:02 +0200

    add theme section

--- src/components/ThemeSection/ThemeSection.tsx
+++ src/components/ThemeSection/ThemeSection.tsx
@@ -7,10 +7,11 @@ import "./style.css"
 interface Props {
 }
 
-function ThemeItem(props: { color: string, name: string }){
+function ThemeItem(props: { backgroundColor: string, color: string, name: string }){
+    const { backgroundColor, color, name } = props;
     return (
-        <div className="">
-
+        <div className="theme-item" style={{ background: backgroundColor, color }}>
+            {name}
         </div>
     );
 }
@@ -21,6 +22,9 @@ export default function ThemeSection(props: Props){
     const renderThemes = ()=>{
         return (
             <div className="theme-item-container">
+                <ThemeItem color="white" name="System" backgroundColor="linear-gradient(-65.79deg, #030303 3.13%, #CBE9FF 68.46%)" />
+                <ThemeItem color="black" name="Light" backgroundColor="#D4E7F6" />
+                <ThemeItem color="white" name="Dark" backgroundColor="black" />
 
             </div>
         );
